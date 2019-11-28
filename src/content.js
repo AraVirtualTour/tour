3d16d52d4b09afaaac7e5367c6452d17f039f849
syri <itsveev@gmail.com>
2019-11-28 14:27:10 +1300

    thursday

--- src/content.js
+++ src/content.js
@@ -2,13 +2,12 @@ const locationContent = [
     { id: '001', time: 20, required: false, title: 'DummyContent1',             src: '001_0_20.jpg' },
     { id: '002', time: 20, required: true,  title: 'Dummy Image 2 Wowee big!',  src: '002_1_20.jpg' },
     { id: '003', time: 20, required: true,  title: 'Intro Text',                src: '003_1_20.txt' },
-    { id: '004', time: 0,  required: true,  title: 'Link 1',                    src: '004_1_0.link' },
+    { id: '004', time: 0,  required: true,  title: 'Link 1',                    src: '004_1_0.url' },
     { id: '005', time: 40, required: true,  title: 'Pano',                      src: '005_1_40_pano.jpg' },
-    { id: '006', time: 0,  required: false, title: 'Link 2',                    src: '006_0_0.link' },
+    { id: '006', time: 0,  required: false, title: 'Link 2',                    src: '006_0_0.url' },
     { id: '007', time: 0,  required: false, title: 'Audio 1',                   src: '007_0_0.wav' },
     { id: '008', time: 0,  required: true,  title: 'Mean Recording',            src: '008_1_0.wav' },
     { id: '009', time: 0,  required: true,  title: 'Based Subtitles',           src: '009_1_0.wav' }
-    //{ id: '010', time: 0,  required: false, title: '',                          src: '010_0_0.ass' }
 ];
 
 function locationContentLoader() {
--- src/tour.js
+++ src/tour.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import locationContentLoader from './content';
 
 import './css/tour.css';
@@ -8,48 +9,55 @@ const content = require.context('./content/', true);
 
 class Text extends React.Component {
   render () {
-    return <p>{this.props.text}</p>;
+    return (
+      <div id={this.props.id}>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.text}</p>
+      </div>
+    );
   }
 }
 
 class Image extends React.Component {
   render () {
-    return <img src={this.props.src} alt={this.props.alt} />;
+    return (
+      <div id={this.props.id}>
+        <img src={this.props.src} alt={this.props.alt} />
+      </div>
+    );
   }
 }
 
 class Panorama extends React.Component {
   render () {
-    return <img src={this.props.src} alt={this.props.alt} />;
+    return (
+      <div id={this.props.id}>
+        <img src={this.props.src} alt={this.props.alt} />
+      </div>
+    );
   }
 }
 
 class Link extends React.Component {
   render () {
-    return <a href={this.props.destination}>{this.props.text}</a>;
+    return (
+      <div id={this.props.id}>
+        <a href={this.props.src} target='_blank' rel="noopener noreferrer">{this.props.text}</a>
+      </div>
+    );
   }
 }
 
 class Audio extends React.Component {
   render () {
-    return <audio src={this.props.src} type={this.props.type} controls />;
+    return (
+      <div id={this.props.id}>
+        <audio src={this.props.src} type={this.props.type} controls />
+      </div>
+    );
   }
 }
 
-function getQueryVariable(variable)
-{
-  let query = window.location.search.substring(1);
-  let vars = query.split("&");
-
-  for (let i = 0; i < vars.length; i++) {
-    let pair = vars[i].split("=");
-    if(pair[0] === variable) {
-      return pair[1];
-    }
-  }
-  
-  return(false);
-}
 
 class Tour extends React.Component {
   constructor(props) {
@@ -62,34 +70,53 @@ class Tour extends React.Component {
   }
 
   componentDidMount() {
-    let location = getQueryVariable('location');
+    let location = this.getQueryVariable('location');
     let locationContent = locationContentLoader();
 
     this.setState({location, locationContent});
   }
 
+  getQueryVariable(variableName) {
+    let query = window.location.search.substring(1);
+    let vars = query.split("&");
+
+    for (let index = 0; index < vars.length; index++) {
+      let pair = vars[index].split("=");
+
+      if (pair[0] === variableName) {
+        return pair[1];
+      }
+    }
+
+    return(false);
+  }
+
   renderContent() {
     let result = [];
+
+    if (!this.state.location) {
+      return result;
+    }
 
     for (let index in this.state.locationContent) {
       let file = this.state.locationContent[index];
       let source = `./${this.state.location}/${file['src']}`;
 
       if (file['src'].includes('jpg')) {
-        result.push(file['src'].includes('_pano') ? <Panorama key={file['id']} src={content(source)} alt={file['title']} /> :
-                                                    <Image key={file['id']} src={content(source)} alt={file['title']} />);
+        result.push(file['src'].includes('_pano') ? <Panorama key={file['id']} id={file['id']} src={content(source)} alt={file['title']} /> :
+                                                    <Image key={file['id']} id={file['id']} src={content(source)} alt={file['title']} />);
       }
 
       if (file['src'].includes('txt')) {
-        result.push(<Text key={file['id']} text={file['title']} />);
+        result.push(<Text key={file['id']} id={file['id']} text={file['title']} />);
       }
-
-      if (file['src'].includes('link')) {
-        result.push(<Link key={file['id']} destination='#' text={file['title']} />);
+      
+      if (file['src'].includes('url')) {
+        result.push(<Link key={file['id']} id={file['id']} src={source} text={file['title']} />);
       }
 
       if (file['src'].includes('wav')) {
-        result.push(<Audio key={file['id']} src={content(source)} type='audio/wav' />);
+        result.push(<Audio key={file['id']} id={file['id']} src={source} type='audio/wav' />);
       }
     }
 
