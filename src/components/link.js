e9dccae029766c825dd8a65dc26150d22e59277d
jok0436 <josiahkerr@hotmail.com>
2019-12-01 18:53:14 +1300

    fixed CORB issue.

--- src/components/link.js
+++ src/components/link.js
@@ -1,39 +1,37 @@
-import React from 'react';
+import React from 'react'
 import ReactPlayer from 'react-player'
 
-import RequiredPoint from './requiredPoint';
-
+import RequiredPoint from './requiredPoint'
 
 export default class Link extends React.Component {
   constructor (props) {
-    super(props);
+    super(props)
 
     this.state = {
       url: ''
-    };
+    }
   }
 
   componentDidMount () {
     fetch(this.props.src)
       .then(response => response.text())
-      .then((response) => this.setState({url: response.match(/http.*/gm)}));
+      .then((response) => this.setState({ url: response.match(/http.*/gm) }))
   }
 
   renderContent () {
-    let url = this.state.url.toString();
+    let url = this.state.url.toString()
 
     if (url.includes('youtube') || url.includes('youtu.be')) {
-      let videoId = url.match(/v=.*/gm).toString();
-
-      return <ReactPlayer url={`https://www.youtube.com/watch?v=${videoId}`} playing />;
+      let videoId = url.match(/v=.*/gm).toString()
+      return <ReactPlayer url={`https://www.youtube.com/watch?${videoId}`} config={{ preload: true }} />
     } else {
-      return <a href={this.state.url} target='_blank' rel="noopener noreferrer">{this.props.text}</a>;
+      return <a href={this.state.url} target='_blank' rel='noopener noreferrer'>{this.props.text}</a>
     }
   }
 
   renderRequired () {
     if (this.props.required) {
-      return <RequiredPoint />;
+      return <RequiredPoint />
     }
   }
 
@@ -43,6 +41,6 @@ export default class Link extends React.Component {
         {this.renderContent()}
         {this.renderRequired()}
       </div>
-    );
+    )
   }
 }
